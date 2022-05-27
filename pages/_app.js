bdaa36ee7342d3e6966d993cad9b401cc71bc29b
Osman Aziz <osmanaziz012@gmail.com>
2022-05-27 22:44:07 +0500

    About Section Responsive Fix (IPAD MINI / AIR MINOR ISSUE)

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,15 @@ import "../styles/globals.css";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <>
       <Head>
--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,15 @@ const Home = () => {
         <div className={`row my-5 py-5 ${styles.about_section}`}>
           <div className="col-md-6 d-lg-block d-flex justify-content-center align-items-center px-0 position-relative ">
             <img
+              data-aos="fade-right"
+              data-aos-duration="1000"
               src="/about_photo_1.jpg"
               alt=""
               className={`${styles.vertical_img} shadow`}
             />
             <img
+              data-aos="fade-up"
+              data-aos-duration="2000"
               src="/about_photo-15.jpg"
               alt=""
               className={`${styles.horizontal_img} d-lg-block d-none`}
@@ -31,16 +35,16 @@ const Home = () => {
               <div className="d-inline-block"></div>
               <h6 className="d-inline-block mt-1 mx-3">ABOUT ME</h6>
             </div>
-            <h1 className="my-4 pb-4 ">
+            <h1 className="my-4 pb-5 me-md-5 text-sm-start text-center">
               I’m Jawad Pervez, a professional photographer based in London.
             </h1>
-            <p className="me-1">
+            <p className="me-sm-2 pe-md-4 me-0 my-4 text-sm-start text-center">
               Mauris accumsan quis purus ut pretium. Suspendisse nunc magna,
               mollis vel pellentesque quis, consectetur eget neque. Phasellus
               posuere lacus lacus. Nunc consectetur, nulla at sagittis aliquam,
               velit diam molestie quam, nec congue orci odio vitae leo.
             </p>
-            <p>MORE ABOUT ME</p>
+            <p className="text-sm-start text-center">MORE ABOUT ME</p>
           </div>
         </div>
       </div>
